659a9f42db1b762aad675e36a09cf524deffb781
Terex98 <mribrahim-1998@hotmail.com>
2023-08-11 17:46:13 -0700

    16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main - This is the entry point of the code
+ *
+ *Return:0 Success
+ */
+int main(void)
+{
+	int a;
+	char n;
+
+	for (a = 0; a < 10; a++)
+		putchar((a % 10) + '0');
+	for (n = 'a'; n <= 'f'; n++)
+		putchar(n);
+	putchar('\n');
+
+	return (0);
+}
