fdc6091869ba6d8fe20dba2f9116453491292b9f
Terex98 <mribrahim-1998@hotmail.com>
2023-08-11 17:50:46 -0700

    last

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ *main - This is the entry point of the code
+ *
+ *Return:0 Success
+ */
+int main(void)
+{
+	int num;
+
+	num = 0;
+
+	while (num <= 9)
+	{
+		putchar(num + '0');
+		if (num < 9)
+		{
+		putchar(',');
+		putchar(' ');
+		}
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
