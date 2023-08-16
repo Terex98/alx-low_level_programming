dd6b1c06106d437558e023df0820c2f36686eeb8
Terex98 <mribrahim-1998@hotmail.com>
2023-08-16 07:00:10 -0700

    s

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
- * print_alphabet - make the alphabet
- * return void
+ * print_alphabet - print the alphabet
+ * Return: Always 0 (Success)
  */
-
 void print_alphabet(void)
 {
 	char c;
