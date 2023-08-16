05042fbb94496fc5d1bee3fba8127237239509df
Terex98 <mribrahim-1998@hotmail.com>
2023-08-16 06:03:08 -0700

    w

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
+int _putchar(char c);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void print_to_98(int n);
+void print_time_table(int n);
 
-int _putchar(char C);
-
-
-#endif 
+#endif
