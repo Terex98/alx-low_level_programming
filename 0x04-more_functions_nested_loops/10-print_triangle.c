5b3e33df7f824a9939914ebea72e45b8555edb51
Terex98 <mribrahim-1998@hotmail.com>
2023-08-18 21:48:19 -0700

    11

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -12,16 +12,16 @@ void print_triangle(int size)
 {
 	int hght, base;
 
-		if (size <= 0)
+		if (size < 0)
 		{
 		_putchar('\n');
 	else
 	{
-		for (hght = 1; hght <= size; hght++)
+		for (hght = 1; hght < size; hght++)
 		{
-			for (base = 1; base <= size; base++)
+			for (base = 1; base < size; base++)
 			{
-				if ((hght + base) <= size)
+				if ((hght + base) < size)
 					_putchar(' ');
 				else
 					_putchar('#');
